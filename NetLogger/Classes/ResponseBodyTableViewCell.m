d2cba4cd7bbe56ae5f7dcac45fcbdbab5c550eb2
Jasmin Ceco <jasminceco@jasmincecomac.local>
2020-05-01 12:27:21 +0200

    copyLable

--- NetLogger/Classes/ResponseBodyTableViewCell.m
+++ NetLogger/Classes/ResponseBodyTableViewCell.m
@@ -11,8 +11,41 @@
 
 - (void)awakeFromNib {
     [super awakeFromNib];
-    // Initialization code
+    _detailtextLabel.userInteractionEnabled = YES;
+    _titletextLabel.userInteractionEnabled = YES;
+UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(textTapped:)];
+UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(textPressed:)];
+    [_titletextLabel addGestureRecognizer:tap];
+    [_titletextLabel addGestureRecognizer:longPress];
+    [_detailtextLabel addGestureRecognizer:tap];
+    [_detailtextLabel addGestureRecognizer:longPress];
 }
+ 
+- (void) textPressed:(UILongPressGestureRecognizer *) gestureRecognizer {
+    if (gestureRecognizer.state == UIGestureRecognizerStateRecognized &&
+        [gestureRecognizer.view isKindOfClass:[UILabel class]]) {
+        UILabel *someLabel = (UILabel *)gestureRecognizer.view;
+        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+        [pasteboard setString:someLabel.text];
+        //let the user know you copied the text to the pasteboard and they can no paste it somewhere else
+        
+    }
+}
+ 
+- (void) textTapped:(UITapGestureRecognizer *) gestureRecognizer {
+    if (gestureRecognizer.state == UIGestureRecognizerStateRecognized &&
+        [gestureRecognizer.view isKindOfClass:[UILabel class]]) {
+        NSLog(@"fjeeddfj");
+        UILabel *someLabel = (UILabel *)gestureRecognizer.view;
+        UIPasteboard *pasteboard = [UIPasteboard generalPasteboard];
+        [pasteboard setString:someLabel.text];
+        //let the user know you copied the text to the pasteboard and they can no paste it somewhere else
+    }
+}
+
+
+
+
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
